7557d39c49ec856aac2bd4776233ac11dea61050
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2025-01-27 07:00:21 +0530

    1111

--- src/components/WhatsCookingSection.js
+++ src/components/WhatsCookingSection.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './../styles/WhatsCookingSection.css';
+import React from "react";
+import "./../styles/WhatsCookingSection.css";
 
 function WhatsCookingSection() {
   return (
     <section className="whats-cooking">
       <div className="container">
         <h2>What's Cooking?</h2>
-        <p>With over [X] years of experience, I've been sharing the authentic tastes of Sri Lankan cuisine with food lovers from around the world. Join me in my kitchen to cook, laugh, and explore the rich heritage of Sri Lanka's food culture.</p>
-        <p>Whether you're craving spicy curries, sweet desserts, or savory snacks, I offer a variety of hands-on cooking classes tailored to your taste.</p>
+        <div className="p">
+          <div>
+            {" "}
+            <p>
+              With over [X] years of experience, I've been sharing the authentic
+              tastes of Sri Lankan cuisine with food lovers from around the
+              world. Join me in my kitchen to cook, laugh, and explore the rich
+              heritage of Sri Lanka's food culture.
+            </p>
+          </div>
+          <div>
+            {" "}
+            <p>
+              Whether you're craving spicy curries, sweet desserts, or savory
+              snacks, I offer a variety of hands-on cooking classes tailored to
+              your taste.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
